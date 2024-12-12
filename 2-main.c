c5b8b89aebcdb9574f7238479f5cfef810a8f150
kevinj83 <kevinj_2005>
2024-12-12 18:08:30 -0400

    fix

--- 2-main.c
+++ 2-main.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-    int array[] = {64, 25, 12, 22, 11};
-    size_t n = sizeof(array) / sizeof(array[0]);
+	int array[] = {64, 25, 12, 22, 11};
+	size_t n = sizeof(array) / sizeof(array[0]);
 
-    print_array(array, n);
-    printf("\n");
-    selection_sort(array, n);
-    printf("\n");
-    print_array(array, n);
-    return (0);
+	print_array(array, n);
+	printf("\n");
+	selection_sort(array, n);
+	printf("\n");
+	print_array(array, n);
+	return (0);
 }
 
