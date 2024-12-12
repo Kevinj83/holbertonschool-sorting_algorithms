00493d38a3bd17f8bffc16afc28120933272934c
kevinj83 <kevinj_2005>
2024-12-12 17:56:21 -0400

    fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,29 +8,29 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, n;
-    int temp;
-    int swapped;
+	size_t i, n;
+	int temp;
+	int swapped;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    for (n = size; n > 1; n--)
-    {
-        swapped = 0;
-        for (i = 0; i < n - 1; i++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                temp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = temp;
-                swapped = 1;
-                print_array(array, size); /* Print array after each swap */
-            }
-        }
-        if (!swapped)
-            break;
-    }
+	for (n = size; n > 1; n--)
+	{
+		swapped = 0;
+		for (i = 0; i < n - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				swapped = 1;
+				print_array(array, size); /* Print array after each swap */
+			}
+		}
+		if (!swapped)
+			break;
+	}
 }
 
