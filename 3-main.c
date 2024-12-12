855abdc026863063bb4d6237bfbafbce6bfe9bf4
kevinj83 <kevinj_2005>
2024-12-12 18:13:50 -0400

    fix

--- 3-main.c
+++ 3-main.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-    int array[] = {10, 80, 30, 90, 40, 50, 70};
-    size_t n = sizeof(array) / sizeof(array[0]);
+	int array[] = {10, 80, 30, 90, 40, 50, 70};
+	size_t n = sizeof(array) / sizeof(array[0]);
 
-    print_array(array, n);
-    printf("\n");
-    quick_sort(array, n);
-    printf("\n");
-    print_array(array, n);
-    return (0);
+	print_array(array, n);
+	printf("\n");
+	quick_sort(array, n);
+	printf("\n");
+	print_array(array, n);
+	return (0);
 }
 
--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -8,10 +8,10 @@
  */
 void quick_sort(int *array, size_t size)
 {
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    quick_sort_rec(array, 0, size - 1, size);
+	quick_sort_rec(array, 0, size - 1, size);
 }
 
 /**
@@ -23,14 +23,14 @@ void quick_sort(int *array, size_t size)
  */
 void quick_sort_rec(int *array, int low, int high, size_t size)
 {
-    int pivot;
+	int pivot;
 
-    if (low < high)
-    {
-        pivot = partition(array, low, high, size);
-        quick_sort_rec(array, low, pivot - 1, size);
-        quick_sort_rec(array, pivot + 1, high, size);
-    }
+	if (low < high)
+	{
+		pivot = partition(array, low, high, size);
+		quick_sort_rec(array, low, pivot - 1, size);
+		quick_sort_rec(array, pivot + 1, high, size);
+	}
 }
 
 /**
@@ -44,31 +44,31 @@ void quick_sort_rec(int *array, int low, int high, size_t size)
  */
 int partition(int *array, int low, int high, size_t size)
 {
-    int pivot = array[high];
-    int i = low - 1, j, temp;
+	int pivot = array[high];
+	int i = low - 1, j, temp;
 
-    for (j = low; j < high; j++)
-    {
-        if (array[j] < pivot)
-        {
-            i++;
-            if (i != j)
-            {
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 
-    if (i + 1 != high)
-    {
-        temp = array[i + 1];
-        array[i + 1] = array[high];
-        array[high] = temp;
-        print_array(array, size);
-    }
-    return (i + 1);
+	if (i + 1 != high)
+	{
+		temp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
+	return (i + 1);
 }
 
